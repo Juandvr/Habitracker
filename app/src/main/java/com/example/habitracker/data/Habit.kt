f92402cc4c1b1b60f854102d639e4c517dc55c16
Juandvr <villabonajuan203@gmail.com>
2026-09-12 19:52:08 -0500

    create habit data class and habit dao

---
+++ app/src/main/java/com/example/habitracker/data/Habit.kt
@@ -0,0 +1,12 @@
+package com.example.habitracker.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "habits")
+data class Habit(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val tile: String,
+    val isCompleted: Boolean = false
+)
